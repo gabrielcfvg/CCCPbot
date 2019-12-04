3daaa1619e1c68c8f9ed1b0b3b1799119f2d1ad0
gabrielcfvg <gabrielcfvg@gmail.com>
2019-12-04 05:46:13 -0300

    CCCPbot_007

--- cccpbot.py
+++ cccpbot.py
@@ -77,7 +77,50 @@ def ler():
         arquivo.close()
         return saida
 
+def tabela():
 
+    dados = ler()
+    saida = ''
+    dados.reverse()
+    
+    saida += '```diff\n-TABELA\n'
+    saida += ('-'*25)+'\n'
+    for A in range(10):
+        saida += str(A+1) + ' dias atrás = ' + str(dados[A][1]) + '\n'
+    saida += ('-'*25)+ '\n```'
+    
+    return saida
+
+def media(num):
+
+    dados = ler()
+    dados.reverse()
+    saida = []
+    for A in range(num):
+        saida.append(int(dados[A][1]))
+
+    media = sum(saida)/num
+
+    return f'a média de mensagens dos ultimos {num} dias é de {media} mensagens.'
+
+def ajuda():
+
+    saida = '''```
+Comandos do Bot
+---------------
+"slava" = O bot responde "Viva a Rússia", usado para fins de teste.
+
+"hoje" = Diz o numero de mensagens postada até o momento atual no dia de hoje.
+
+"atras []" = Esse comado, quando acompanhado de um numero, diz quantas mensagens foram mandadas no dia em especifico,"atras 1" irá mostrar o numero de mensagens postadas ontem.
+
+"tabela" = Esse comando mostra uma tabela com as mensagens dos ultimos 10 dias.
+
+"media []" = Esse comando, quando acompanhado de um numero(assim como o "atras") mostrará a media das mensagens do dia correspondente ao numero que você escolheu até agora, pode ser utilizado com a palavra "tudo" no lugar de um numero, assim ele mostrará a media de todas as mensagens desde o inicio.
+```
+'''
+
+    return saida
 #====================================================================================================================================================================
 
 temporizador.start()
@@ -121,10 +164,29 @@ async def on_message(message):
 
             elif message.content.startswith('ajuda'):
 
-                await message.channel.send('Comandos do Bot\n"slava" = o bot responde "Viva a Rússia", usado para fins de teste.\n"hoje" = diz o numero de mensagens postada até o momento atual no dia de hoje.\n"atras" = esse comado, quando acompanhado de um numero, diz quantas mensagens foram mandadas no dia em especifico,"atras 1" irá mostrar o numero de mensagens postadas ontem.')
-        except:
-            await message.channel.send('Você digitou errado, camarada!\nDigite "ajuda" para ver os comandos disponiveis!')
+                await message.channel.send(ajuda())
 
+            elif message.content.startswith('tabela'):
+                
+                await message.channel.send(tabela())
+
+            elif message.content.startswith('media'):
+
+                numm = message.content[6:]
+
+                if numm == 'tudo':
+
+                    numm = len(ler())
+
+                elif numm.isnumeric() == True:
+                    numm = int(numm)
+
+                await message.channel.send(media(numm))
+
+                
+        except Exception as error:
+            await message.channel.send('Você digitou errado, camarada!\nDigite "ajuda" para ver os comandos disponiveis!')
+            print(error)
     
     #elif str(message.channel) == 'escriba-javâliano' and message.author.id == 647455830530326533:
         #await message.channel.send('Viva a Rússia, seu yanke imperialista')
